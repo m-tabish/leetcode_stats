0f6bc7ed1a6efc867e26a33f3e536cd2e94d2743
Tabish <mohdtabishkhan001@gmail.com>
2024-01-13 16:53:46 +0530

    Updated package.json

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,29 +1,29 @@
 import { useContext } from "react";
 
 import UserContext from "../Context/UserContext";
-function Heading() {
-	const { entry } = useContext(UserContext);
-	// const apiUrl = `https://leetcode-stats-api.herokuapp.com/${entry}`;
 
-	if (!entry) {
-		return (
-			<h1 className="text-5xl ">
-				{" "}
-				&lt; <u>Welcome to Leetcode_stats</u> &gt;
-			</h1>
-		);
-	}
-	return (
-		<>
-			<div>
-				<div className=" text-5xl text-center font-bold  p-3 border-gray-100/30 border-1 rounded-xl ">
-					<div className="opacity-100">
-						{entry !== null ? `${entry}'s Leetcode Stats` : `Enter Name`}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+function Heading() {
+  const { entry } = useContext(UserContext);
+
+  if (!entry || entry === "") {
+    return (
+      <h1 className="text-5xl">
+        &lt; <u>Welcome to Leetcode_stats</u> &gt;
+      </h1>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <div className="text-5xl text-center font-bold p-3 border-gray-100/30 border-1 rounded-xl">
+          <div className="opacity-100">
+            {entry !== null ? `${entry}'s Leetcode Stats` : `Enter Name`}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Heading;
